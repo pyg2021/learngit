2d8dcc7892d33d5f3d291d191c5a4d1c9fe4b3ab
pengyaoguang <pengyaoguang2021@163.com>
2022-04-07 12:04:38 +0800

    web app

---
+++ awesome-python3-webapp/www/app.py
@@ -0,0 +1,18 @@
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+
+async def index(request):
+    return web.Response(text='Hello world')
+
+async def init_app():
+    app = web.Application()
+    app.add_routes([
+        web.get('/', index)
+    ])
+    return app
+
+loop = asyncio.get_event_loop()
+app = loop.run_until_complete(init_app())
+web.run_app(app, port='9086', host='127.0.0.1')
